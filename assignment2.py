c5fe779ff1c69e8c53fe73ae1481af93a141b045
arpitgarg1550 <arpitgarg1550@gmail.com>
2018-08-21 01:11:39 +0530

    assignment 2

---
+++ assignment2.py
@@ -0,0 +1,24 @@
+#Q=1
+print("I am arpit")
+#Q=2
+print('arpit'+'garg')
+#Q=3
+a=int(input('Enter the value of x\n'))
+b=int(input('Enter the value of y\n'))
+c=int(input('Enter the value of z\n'))
+print(a)
+print(b)
+print(c)
+#Q=4
+print("Let's get started")
+#Q=5
+s="Acadview"
+print(s)
+course="Python"
+print(course)
+fees="5000"
+print(fees)
+#Q=6
+r = int(input("Enter a number: "))
+area = 3.14*r*r
+print(area)
